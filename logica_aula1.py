8a889f55fd07fb954a33cd996a9b996431d52f81
Luciana Hitomi Ikenaga <luciana.hiken@gmail.com>
2026-08-09 18:44:33 -0300

    Adiciona aula 1 de logica

---
+++ logica_aula1.py
@@ -0,0 +1,16 @@
+# Exemplo 1 - Entrada e saída
+nome = input("Qual é o seu nome? ")
+print("Olá,", nome)
+# Exemplo 2 - Variável numérica
+idade = 30
+print(idade)
+# Exemplo 3 - Entrada de número e cálculo
+idade = int(input("Qual é a sua idade? "))
+print("Daqui a 5 anos você terá", idade + 5)
+# Desafio 1 - Daqui a 10 anos
+nome = input("Qual é o seu nome? ")
+idade = int(input("Qual é a sua idade? "))
+print(idade + 10)
+print("Olá,", nome)
+print("Daqui a 10 anos você terá", idade + 10)
+# Aula 1 - Variáveis, entrada e saída
