6206fd4d1bf8ffd41520c9f17a6b45f0579805e9
Luciana Hitomi Ikenaga <luciana.hiken@gmail.com>
2026-08-10 13:46:10 -0300

    Adiciona aula 2 de Python

---
+++ aula_02.py
@@ -0,0 +1,10 @@
+nome = input("Qual é o seu nome? ")
+idade = int(input("Qual é a sua idade? "))
+print("Olá,", nome)
+print("Você tem", idade, "anos")
+ano_nascimento = 2026 - idade
+print("Você nasceu aproximadamente em", ano_nascimento)
+
+
+
+
