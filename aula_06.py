d8e2906dd43d7428c1247c7e1ccca4f45717de88
Luciana Hitomi Ikenaga <luciana.hiken@gmail.com>
2026-08-21 15:00:45 -0300

    Adiciona Aula 6 - operações matemáticas

---
+++ aula_06.py
@@ -0,0 +1,32 @@
+numero1 = 10
+numero2 = 5
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
+
+preco = 100
+quantidade = 3
+
+total = preco * quantidade
+print("Total:", total)
+
+preco = float(input("Qual o preco? "))
+quantidade = int(input("Qual a quantidade? "))
+
+total = preco * quantidade
+
+print("Total:", total)
+
+preco_primeiro_produto = float(input("Digite o preço do primeiro produto: "))
+preco_segundo_produto = float(input("Digite o preço do segundo produto: "))
+
+total = preco_primeiro_produto + preco_segundo_produto
+
+print("Total:", total)
